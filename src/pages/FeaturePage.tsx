34e4b521bc59b5b31ba3959c7b9dc58c70570e9a
Alexander Galichenko <alexandr.galichenko@gmail.com>
2022-12-13 09:53:28 +0300

    Add feature title (#2)
    added feature title
    added multiline and data table

--- src/pages/FeaturePage.tsx
+++ src/pages/FeaturePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FlexSpacer, FlexRow, TextInput, Switch } from '@epam/promo';
+import { FlexSpacer, FlexRow, TextInput, Switch, Text, Badge } from '@epam/promo';
 import { useParams } from 'react-router-dom';
 import { Scenario } from '../components/Scenario';
 import css from "../App.module.scss";
@@ -16,9 +16,17 @@ const filterScenarios = (
     })
 }
 
-export const FeaturePage = (props: any) => {
+const FeatureTitle = (props: any) => <FlexRow padding='12' vPadding='12'>
+    {/*<Panel cx={css.featureTitle} background='white' shadow>*/}
+        <Text font='sans-semibold' cx={css.featureTitle}>Feature: {props.feature.name}</Text>
+        {props.feature.tags.map((tag: any, index: number) => <Badge cx={css.tagBadge} key={index} size='18' color='blue' fill='semitransparent' caption={tag.name} />)}
+    {/*</Panel>*/}
+</FlexRow>
+
+export const FeaturePage = () => {
     const { id }: { id: string } = useParams();
-    const feature = id
+    const isSingleFeatureView = !!id;
+    const feature = isSingleFeatureView
         ? window.data.find((feature: any) => feature.id === id)
         : window.data.reduce((failedScenarios: any, feature: any) => {
             failedScenarios.elements.push(...feature.elements.filter((scenario: any) => scenario.isFailed))
@@ -28,6 +36,7 @@ export const FeaturePage = (props: any) => {
     const [showOnlyFailed, setShowOnlyFailed] = useState(false);
 
     return <>
+        {isSingleFeatureView && <FeatureTitle feature={feature}/>}
         <FlexRow padding='12' vPadding='12'>
             {/*@ts-ignore*/}
             <TextInput cx={css.searchInput} value={ searchValue } onValueChange={ setSearchValue }  placeholder='Search'/>
