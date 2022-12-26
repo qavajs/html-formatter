ba3f726e312a9e98380211ad3087f0b8d339a5ed
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-12-26 15:21:48 +0300

    added clipboard return type

--- src/utils/clipboard.ts
+++ src/utils/clipboard.ts
@@ -1,5 +1,5 @@
-function clipboard(data: string) {
-    return function (event: Event) {
+function clipboard(data: string): Function {
+    return function (event: Event): Promise<void> {
         event.stopPropagation();
         return navigator.clipboard.writeText(data);
     }
