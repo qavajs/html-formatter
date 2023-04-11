8e20782ccf545d319dd928dace6d4d59604ba39d
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-04-11 14:29:58 +0300

    fixed issue of not escaped chars in logs (#24)

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -5,11 +5,13 @@ class HTMLFormatter extends JsonFormatter {
 
     constructor(options) {
         super(options);
-        this.metadata = options.parsedArgvOptions.htmlConfig?.metadata;
+        this.metadata = options.parsedArgvOptions.htmlConfig?.metadata ?? {};
         const log = this.log.bind(this);
         this.log = function(json) {
             const htmlTemplate = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
-            log(htmlTemplate.replace('METADATA', JSON.stringify(this.metadata)).replace('SOURCE_DATA', json));
+            log(htmlTemplate
+                .replace('METADATA', JSON.stringify(this.metadata, null, 0))
+                .replace('SOURCE_DATA', JSON.stringify(JSON.parse(json), null, 0)));
         }
     }
 
--- test/compile_dev.js
+++ test/compile_dev.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 const template = fs.readFileSync('./public/index.html', 'utf-8');
-const json = fs.readFileSync('./test/report.json', 'utf-8');
+const json = require('./report.json');
 
-fs.writeFileSync('./public/index.html', template.replace('SOURCE_DATA', json).replace('METADATA', '[]'));
+fs.writeFileSync('./public/index.html', template
+    .replace('SOURCE_DATA', JSON.stringify(json, null, 0))
+    .replace('METADATA', JSON.stringify({}, null, 0))
+);
