62be9db8c23096540e405149435904152a3d3a34
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-04-11 14:25:09 +0300

    fixed issue of not escaped chars in logs

--- test/compile_report.js
+++ test/compile_report.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 const template = fs.readFileSync('./build/index.html', 'utf-8');
-const json = fs.readFileSync('./test/report.json', 'utf-8');
+const json = require('./report.json');
 
-fs.writeFileSync('./test/report.html', template.replace('SOURCE_DATA', json).replace('METADATA', '[]'));
+fs.writeFileSync('./test/report.html', template
+    .replace('SOURCE_DATA', JSON.stringify(json, null, 0))
+    .replace('METADATA', JSON.stringify({}, null, 0))
+);
