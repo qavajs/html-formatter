8467e64e05dda17b79be52dc22e0c529ed70dfdd
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-01-16 18:55:11 +0200

    fixed multiple tags displaying

--- src/components/FeatureTitle.tsx
+++ src/components/FeatureTitle.tsx
@@ -1,16 +1,8 @@
-import {Badge, FlexRow, Text} from '@epam/promo';
+import {FlexRow, Text} from '@epam/promo';
 import css from '../App.module.scss';
-import clipboard from '../utils/clipboard';
+import {TagList} from './TagList';
 
 export const FeatureTitle = (props: any) => <FlexRow padding='12' vPadding='12'>
     <Text font='sans-semibold' cx={css.featureTitle}>Feature: {props.feature.name}</Text>
-    {props.feature.tags.map((tag: any, index: number) => <Badge
-        cx={css.tagBadge}
-        key={index}
-        size='18'
-        color='blue'
-        fill='semitransparent'
-        caption={tag.name}
-        onClick={clipboard(tag.name)}
-    />)}
+    <TagList tags={props.feature.tags} itemsToRender={10}/>
 </FlexRow>
